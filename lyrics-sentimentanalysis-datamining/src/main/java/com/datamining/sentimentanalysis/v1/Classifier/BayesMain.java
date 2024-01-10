5f84b9588d20b5473a447a8812164b9cf962541b
Calin Nemes <calin.nemes@partner.bmwgroup.com>
2024-01-10 17:27:50 +0200

    naive bayes main

--- lyrics-sentimentanalysis-datamining/src/main/java/com/datamining/sentimentanalysis/v1/Classifier/BayesMain.java
+++ lyrics-sentimentanalysis-datamining/src/main/java/com/datamining/sentimentanalysis/v1/Classifier/BayesMain.java
@@ -1,13 +1,19 @@
 package com.datamining.sentimentanalysis.v1.Classifier;
 
-import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class BayesMain {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
+        // path for the set used for training
         String indexPath = "trainingSet.txt";
 
-        NaiveBayes naiveBayes = new NaiveBayes(indexPath, true, true ,true ,true);
+        NaiveBayes naiveBayes = new NaiveBayes(indexPath, true, false ,true ,true);
         naiveBayes.train();
 
+        //set used for testing
+        naiveBayes.classifyFile("testingSet.txt");
+
+        // final Map<String, Integer> stringIntegerMap = naiveBayes.classifyFile("testingSet.txt");
+        // System.out.println(stringIntegerMap);
     }
 }
